c9202786be0e018a52ed6f19c7dcfc9f53d2cefa
jiwonlee1223 <to55540750@gmail.com>
2024-11-30 04:53:06 +0900

    modified:   app.js 	modified:   public/index.html

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ function handleSocketConnection(socket) {
 
 io.on("connection", handleSocketConnection);
 
-const PORT = 3002;
+const PORT = 80;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
